ebc02f6b67e14643f21a17adde4ea267f67f4ea4
rocket12213 <rocket12213@gmail.com>
2020-04-09 23:08:35 +0900

    add models & remove vote app

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    code = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = 'users'
+
--- we_hackathon/settings.py
+++ we_hackathon/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'vote',
     'user',
     'artwork'
 ]
