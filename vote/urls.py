d4f22074607ac8a77e1fd5d893eeed8b75dd75c6
soheon-lee <lsheon93@gmail.com>
2020-04-10 05:09:20 +0900

    MyVoteNumView 등록

--- vote/urls.py
+++ vote/urls.py
@@ -3,11 +3,12 @@ from .views      import (
     VoteView,
     ResultView,
     ArtworkView,
+    MyVoteNumView,
 )
 
 urlpatterns = [
     path('vote', VoteView.as_view()),
-    path('vote/<int:artwork_id>', VoteView.as_view()),
+    path('vote/<int:category_id>', MyVoteNumView.as_view()),
     path('result/<int:category_id>', ResultView.as_view()),
     path('artwork/<int:category_id>', ArtworkView.as_view())
 ]
